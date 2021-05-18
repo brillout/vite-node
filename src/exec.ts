9eace1fa648c79aa4088f7f2bf046b11776dce1a
Romuald Brillout <git@brillout.com>
2021-05-18 21:46:06 +0200

    wip

--- src/exec.ts
+++ src/exec.ts
@@ -4,38 +4,38 @@ import * as WebSocket from "ws";
 
 export { execVite as exec };
 
-let entry: string
+let entry: string;
 async function execVite(_entry: string) {
-  entry = _entry
-  start()
+  entry = _entry;
+  start();
   listen();
 }
 
-var proc: ChildProcess
+var proc: ChildProcess;
 function start() {
   const cwd = process.cwd();
   const entryPath = join(cwd, entry);
-  proc =  spawn("node", [join(__dirname, "./child"), entryPath], {
+  proc = spawn("node", [join(__dirname, "./child"), entryPath], {
     cwd,
     detached: false,
     stdio: "inherit",
   });
 }
 function kill() {
-  proc.kill()
+  proc.kill();
 }
 
 function listen() {
   setTimeout(() => {
-  const ws = new WebSocket("ws://localhost:24678");
-  ws.on("open", function open() {
-    ws.send("something");
-  });
+    const ws = new WebSocket("ws://localhost:24678");
+    ws.on("open", function open() {
+      ws.send("something");
+    });
 
-  ws.on("message", function incoming(data) {
-    console.log('d', data);
-    kill()
-    start()
-  });
-  }, 3*1000)
+    ws.on("message", function incoming(data) {
+      console.log("d", data);
+      kill();
+      start();
+    });
+  }, 3 * 1000);
 }
